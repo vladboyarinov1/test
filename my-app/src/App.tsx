7951129e7a8a645ca358b2139e7f9abff5a30be1
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-02-01 02:49:26 +0300

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>TEST</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
